da4cb54c7679332ca3de07e5c5d99ca55158d96f
shovon tanvir <shovontanvir@shovons-MacBook-Pro.local>
2023-07-04 11:33:02 +0600

    navbar position fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/images/logo-web.png";
 
 const Navbar = () => {
   return (
-    <nav className="w-screen bg-transparent z-50  px-20 py-10">
+    <nav className="w-screen bg-transparent z-50  px-20 py-10 fixed">
       <img src={logo} alt="logo" />
     </nav>
   );
